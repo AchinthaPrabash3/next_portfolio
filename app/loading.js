330fb6e1ad70e494b672fbbdb450b90d18056640
achinthaprabash3 <achinthaprabash3@gmail.com>
2025-05-26 23:42:03 +0530

    fix

--- app/loading.js
+++ app/loading.js
@@ -4,7 +4,7 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 const Loading = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      <AiOutlineLoading3Quarters className=" animate-spin" />
+      <AiOutlineLoading3Quarters className=" animate-spin size-24" />
     </div>
   );
 };
