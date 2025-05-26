2eac694f8f0a1fd922f2babc14f3cab88eda6007
achinthaprabash3 <achinthaprabash3@gmail.com>
2025-05-26 19:52:19 +0530

    fixed a bug

--- components/projectCard.js
+++ components/projectCard.js
@@ -1,12 +1,14 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
 import Image from "next/image";
 
-const ProjectCard = async ({data}) => {
-    const {name,tech,img,des} = data;
-    return (
-       <div className="cursor-pointer select-none hover:scale-95 transition-all duration-300">
-           <img src={img.src} />
-       </div>
-    )
-}
-export default ProjectCard
+const ProjectCard = async ({ data }) => {
+  const { name, tech, img, des } = data;
+  return (
+    <div className="cursor-pointer select-none hover:scale-95 transition-all duration-300">
+      <img src={img.src} />
+    </div>
+  );
+};
+export default ProjectCard;
